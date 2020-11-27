17bd56c8365ec49825a86c481b23499697d4055d
awcleveland98 <74992775+awcleveland98@users.noreply.github.com>
2020-11-27 12:18:58 -0600

    Update thread.h

--- lab6/thread.h
+++ lab6/thread.h
@@ -11,6 +11,6 @@ void rc4_init( unsigned char *key, int key_len);
 void rc4( unsigned char* key,  int key_len,
                    char* buff, int len);
 
-void threads();
+void threads(unsigned char *tkey, char *sBuff);
 
 #endif
