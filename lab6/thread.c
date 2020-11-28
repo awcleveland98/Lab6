e7f3a6e3abcab3235b97ecf3945281f7b4ef8c34
awcleveland98 <74992775+awcleveland98@users.noreply.github.com>
2020-11-28 17:51:06 -0600

    Update thread.c

--- lab6/thread.c
+++ lab6/thread.c
@@ -29,10 +29,9 @@ void rc4_init( unsigned char *key, int key_len){
     }
 }
 
-void rc4( unsigned char* key,  int key_len,
-                   char* buff, int len){   
-    
-    
+void rc4( unsigned char* key,  int key_len, char* buff, int len){   
+
+    pthread_mutex_lock(&task_mutex);
     rc4_init(key,key_len);
     //process a byte at a time
     unsigned long t1=0,t2=0;
@@ -45,9 +44,62 @@ void rc4( unsigned char* key,  int key_len,
         *buff=out;        
         buff++;        
    }
+   pthread_mutex_unlock(&task_mutex);
 }
 
-void threads(unsigned char *tkey, char *sBuff){
+void *work(void *arg)
+{
+    Thread *tskcur = arg;
+    TQueue *queue = tskcur->queue;
+    Task *t;
+    unsigned char key[256], message[256], originalmessage[256];
+    int keylen, meslen;
+
+    FILE *file = fopen("message.txt", "rb");
+		fgets(originalmessage, 256, (FILE*)file);
+		fclose(file);
+
+    while (1) {
+        t = dequeue(queue);
+
+        if (t == NULL)
+            break;
+        
+        for (int i = 0; i < 256; i++)
+        {
+            if(t->key[i] != 0)
+                key[i] = t->key[i];
+        }
+        for (int i = 0; i < 256; i++)
+        {
+            if(t->message[i] == 0)
+                break;
+            
+            message[i] = t->message[i];
+        }
+        
+        keylen = sizeof(key);
+        meslen = strlen(message);
+
+        rc4(key, keylen, message, meslen);
+
+        printf("Message Printed by thread: %d\n", tskcur->xid);
+        for (int i = 0; i < meslen; i++){
+            printf("%c", message[i]);
+        }
+
+        printf("\n\n");
+
+        if (strcmp(originalmessage, message) == 0)
+        {
+            printf("Foumd it");
+            break;
+        }
+    }
+    return (void *) 0;
+}
+
+void threads(unsigned int *tkey, char *sBuff){
     TQueue *queue;
     Task *t;
     Thread *tsk;
@@ -61,7 +113,7 @@ void threads(unsigned char *tkey, char *sBuff){
     Thread threads[opt_T];
 
     if (opt_Q == 0)
-        opt_Q = 100;
+        opt_Q = 10000;
     t = malloc(sizeof(Task) * opt_Q);
 
     for (int i = 0; i < opt_Q; i++) {
